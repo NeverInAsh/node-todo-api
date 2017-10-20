cf71c5fba186733f7d376f347488a3a47b8124a4
Anshik Anshik <anshik.anshik@zs.com>
2017-10-20 22:40:00 +0530

    add and delete mongodb

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,22 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/toDoApp',(err, db) =>{
+	if(err){
+		console.log("Unable to connect to database");
+	}else{
+
+	}
+	// //deleteMany
+	// db.collection('todoDB').deleteMany({text: 'Play Squash'}).then((result)=>{
+	// 	console.log(result);// we can see properties ok =1 and n =3(# of entries)
+	// })
+	// //deleteOne
+	// db.collection('todoDB').deleteOne({text: 'Play Squash'}).then((result)=>{
+	// 	console.log(result);
+	// })
+	//findOneAndDelete
+	db.collection('todoDB').findOneAndDelete({completed:false}).then((result)=>{
+		console.log(result);
+	})
+	//db.close();
+})
