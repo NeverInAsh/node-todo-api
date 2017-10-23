8c49fbcab9a5d75448a6d1c5b9d83ac070b9563b
Anshik Anshik <anshik.anshik@zs.com>
2017-10-23 20:17:06 +0530

    Add test cases for GEt /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server.js');
 const {toDolistDB} = require('./../models/todo.js');
 
 const todos = [{
+	_id:new ObjectID(),
 	text: 'First customary todo'
 },{
+	_id:new ObjectID(),
 	text: 'Second customary todo'
 }]
 
@@ -75,4 +78,36 @@ describe('GET/todos', () =>{
 		})
 		.end(done);//Since we r not doing anything asynchronously
 	});
+});
+
+describe('GET /todos/:id', () =>{
+	it('should return todo doc', (done)=>{
+		request(app)
+		.get(`/todos/${todos[0]._id.toHexString()}`)
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.todo_id.text).toBe(todos[0].text);
+		})
+		.end(done);
+
+	it('shoudl return 404 if todo not found', (done) =>{
+		var newID = new ObjectID();
+		request(app)
+		.get(`/todos/${newID.toHexString()}`)
+		.expect(404)
+		.end(done)
+
+	});
+
+	it('shoudl return 404 for non object id', (done) =>{
+		var newID = '1234';
+		request(app)
+		.get(`/todos/${newID}`)
+		.expect(404)
+		.end(done)
+
+	});
+
+	});
+
 });
