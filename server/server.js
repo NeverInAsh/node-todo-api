5b016f6268bfe66cffe23b1f9181dac5ea651549
Anshik Anshik <anshik.anshik@zs.com>
2017-10-23 19:49:36 +0530

    Add get todos id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {toDolistDB} = require('./models/todo');
 var {userlistDB} = require('./models/users');
@@ -33,6 +35,25 @@ app.get('/todos', (req, res) =>{
 	},(err)=>{
 		res.status(400).send(err);
 	});
+
+});
+
+app.get('/todos/:id', (req, res) =>{ //id param will be on req object
+	var id = req.params.id;
+	//res.send(req.params);
+	if(ObjectID.isValid(id)){
+		toDolistDB.findById(id).then((todo_id) =>{
+			if(!todo_id){
+				return res.status(404).send();
+			}
+			res.status(200).send({todo_id});
+		}).catch((e) =>{
+			res.status(400).send();
+		});
+
+	}else{
+		res.status(404).send()
+	}
 
 });
 
